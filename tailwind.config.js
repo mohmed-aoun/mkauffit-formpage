4fc6b5d3f0c56c2b3ad74416dba4f100cb3d5ad4
mohmed-aoun <mohmedaoun29@gmail.com>
2025-11-01 19:21:09 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-export default {};
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-orange': '#ff8c42',
+        'brand-orange-dark': '#e67e31',
+        'brand-gray-light': '#f5f5f5',
+        'brand-gray': '#d1d5db',
+        'brand-gray-text': '#6b7280',
+        'brand-text': '#1f2937',
+      },
+    },
+  },
+};
