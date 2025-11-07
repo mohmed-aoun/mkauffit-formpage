1e3ff03906ac35520e68c04f7cd6cfdf3a0b2bec
mohmed-aoun <mohmedaoun29@gmail.com>
2025-11-07 15:27:31 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,21 @@ import '@/app/globals.css';
 export const metadata: Metadata = {
   title: 'Health Coaching: Pre-Consultation Form',
   description: 'Health coaching pre-consultation form to gather your health journey details',
+  icons: {
+    icon: [
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icons/favicon.ico' },
+    ],
+    apple: [
+      { url: '/icons/apple-touch-icon.png', sizes: '180x180' },
+    ],
+    other: [
+      { rel: 'android-chrome', url: '/icons/android-chrome-192x192.png', sizes: '192x192' },
+      { rel: 'android-chrome', url: '/icons/android-chrome-512x512.png', sizes: '512x512' },
+      { rel: 'manifest', url: '/icons/site.webmanifest' },
+    ],
+  },
 };
 
 export default function RootLayout({
