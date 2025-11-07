2f57a721bef0e60b7eac57d71d61928d855adee2
mohmed-aoun <mohmedaoun29@gmail.com>
2025-11-07 23:25:26 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     icon: [
       { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/icons/favicon.ico', type: 'image/x-icon' },
+      { url: '/icons/favicon.ico?v=5', type: 'image/x-icon' },
     ],
     apple: '/icons/apple-touch-icon.png',
   },
